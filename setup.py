2b10ef541e85e955e46e772419c84f1286b9a6aa
marc <mgirona29@gmail.com>
2024-03-12 10:44:51 +0000

    set up repo with cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='probabilistic downscaling of daily precipitation in mountains',
+    author='Marc Girona-Mata',
+    license='MIT',
+)
